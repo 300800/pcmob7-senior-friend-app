c5651a667a4309349c3892b4d55991c9f9015166
grace <gracechongbl@gmail.com>
2021-01-26 11:39:25 +0800

    create array of numbers

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Linking,
+  Alert,
+  Platform,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const numbers = ["97661000", "90403905", "98780000"];
+
+  const renderImage = () => {
+    numbers.map((number) => {
+      return (
+        <TouchableOpacity onPress={() => Linking.openURL(`tel:${number}`)}>
+          <Image
+            style={{ width: 100, height: 100, marginBottom: 15 }}
+            source={{
+              uri:
+                "https://static01.nyt.com/images/2017/07/27/us/27techfix/27techfix-videoSixteenByNineJumbo1600-v2.jpg",
+            }}
+          />
+        </TouchableOpacity>
+      );
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>SENIOR FRIEND APP</Text>
       <StatusBar style="auto" />
+      {renderImage()}
     </View>
   );
 }

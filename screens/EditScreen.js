1ba547f9e8b8a7da2a84caf58312347fa806bd3d
grace <gracechongbl@gmail.com>
2021-02-01 11:26:16 +0800

    installed react-navigation/native

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StylesSheet, Text, View } from "react-native";
 import { commonStyles } from "../styles/commonStyles";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function EditScreen({ navigation }) {
   return (
